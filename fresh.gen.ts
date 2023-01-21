9b223d973900263a797786bbde58f6532ca72444
Marcelo Suarez <amsuarez99@gmail.com>
2023-01-21 17:34:42 -0600

    rerun for manifest

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -8,7 +8,6 @@ import * as $1 from "./routes/blog/[id].tsx";
 import * as $2 from "./routes/blog/index.tsx";
 import * as $3 from "./routes/index.tsx";
 import * as $4 from "./routes/search.tsx";
-import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
@@ -18,9 +17,7 @@ const manifest = {
     "./routes/index.tsx": $3,
     "./routes/search.tsx": $4,
   },
-  islands: {
-    "./islands/Counter.tsx": $$0,
-  },
+  islands: {},
   baseUrl: import.meta.url,
   config,
 };
